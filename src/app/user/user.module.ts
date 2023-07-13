7574fb61d8c32a22f5ba6d56b49f910db8ea6018
wtsuzuki <willian.suzuki1993@gmail.com>
2023-07-13 20:07:11 -0300

    Criado componente responsável por cadastro de novo usuario

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
-
+import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthModalComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    AuthModalComponent
   ]
 })
 export class UserModule { }
