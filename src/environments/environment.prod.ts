b813d0349f979208715cf68f43441de7c139f722
wtsuzuki <willian.suzuki1993@gmail.com>
2023-07-14 16:26:20 -0300

    Adicionado Firebase Config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMWsO4-nOe3oD4V4FdSrIE2ibf-YJEC8g",
+    authDomain: "clips-d1faa.firebaseapp.com",
+    projectId: "clips-d1faa",
+    storageBucket: "clips-d1faa.appspot.com",
+    appId: "1:238575371479:web:9949480c12aa924737dc92"
+  }
+};
