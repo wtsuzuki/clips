f419c4660de2a9fd98e977e30f1f22ca09eb3fae
wtsuzuki <willian.suzuki1993@gmail.com>
2023-07-14 10:22:18 -0300

    Adicionado Template form no componente de login

---
+++ src/app/user/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  credentials = {
+    email: '',
+    password: ''
+  }
+
+  login() {
+    console.log(this.credentials);
+    return false;
+  }
+}
